86ea8d2954563512210718689c98da597c4da494
mangal bodele <mangalbodele123@example.com>
2024-11-09 00:57:47 +0530

    update backend

--- my_portfolio/my_portfolio/settings.py
+++ my_portfolio/my_portfolio/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['yourapp.up.railway.app', '127.0.0.1']
+
 
 
 
